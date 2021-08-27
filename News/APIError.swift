9cd2253703b1023f493ae2443ca1ed171790a65a
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-08-27 13:04:41 +0100

    Endpoints and APIError configured.

---
+++ News/APIError.swift
@@ -0,0 +1,27 @@
+//
+//  APIError.swift
+//  APIError
+//
+//  Created by Charlie Nguyen on 27/08/2021.
+//
+
+import Foundation
+
+enum APIError: Error {
+    case decodingError
+    case errorCode(Int)
+    case unknown
+}
+
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .decodingError:
+            return "Failed to decode the object from the service"
+        case .errorCode(let code):
+            return "\(code) - Something went wrong"
+        case .unknown:
+            return "The error is unknown"
+        }
+    }
+}
---
+++ News/Endpoint/NewsEndpoint.swift
@@ -0,0 +1,37 @@
+//
+//  NewsEndpoint.swift
+//  NewsEndpoint
+//
+//  Created by Charlie Nguyen on 27/08/2021.
+//
+
+import Foundation
+
+
+protocol APIBuilder {
+    var urlRequest: URLRequest { get }
+    var baseURL: URL { get }
+    var path: String { get }
+}
+
+enum NewsAPI {
+    case getNews
+}
+
+extension NewsAPI: APIBuilder {
+    var baseURL: URL {
+        switch self {
+        case .getNews:
+            return URL(string: "https://api.lil.software")!
+        }
+    }
+    
+    var path: String {
+        return "/news"
+    }
+    
+    var urlRequest: URLRequest {
+        return URLRequest(url: self.baseURL.appendingPathComponent(self.path))
+    }
+    
+}
