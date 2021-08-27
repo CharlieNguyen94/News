f3d6fc08fd5531d811a753902b2518a0a5e28ee2
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-08-27 12:33:09 +0100

    Initial Commit

---
+++ News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by Charlie Nguyen on 27/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
