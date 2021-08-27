2d9f5738b832661db74c734ac883a188df4b01d1
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-08-27 12:50:59 +0100

    Set up configuration.

---
+++ News/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  HomeView
+//
+//  Created by Charlie Nguyen on 27/08/2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
