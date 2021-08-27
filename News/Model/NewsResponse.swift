4c06e5bef915130658bd752c1941b99c26d399ff
CharlieNguyen94 <charlietugnuyen@hotmail.com>
2021-08-27 12:55:09 +0100

    Model implemented.

---
+++ News/Model/NewsResponse.swift
@@ -0,0 +1,30 @@
+//
+//  NewsResponse.swift
+//  NewsResponse
+//
+//  Created by Charlie Nguyen on 27/08/2021.
+//
+
+import Foundation
+
+// MARK: - News Response
+struct NewsResponse: Codable {
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable, Identifiable {
+    let id = UUID()
+    let author: String?
+    let url: String
+    let source, title: String
+    let articleDescription: String?
+    let image: String?
+    let date: Date
+
+    enum CodingKeys: String, CodingKey {
+        case author, url, source, title
+        case articleDescription = "description"
+        case image, date
+    }
+}
